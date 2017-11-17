070a50b9c53eeabe02e456d930ce47637fed6bad
odtThomas <t.denotter@graafschapcollege.nl>
2017-11-17 12:47:03 +0100

    build init function

--- js/app.js
+++ js/app.js
@@ -1,31 +1,35 @@
-/*
- * Create a list that holds all of your cards
- */
+function init() {
+    //  Create a list that holds all of your cards based on there font-awesome name.
 
+    var cards = ['facebook', 'facebook', 'twitter', 'twitter', 'twitch', 'twitch', 'instagram', 'instagram', 'pied-piper-pp', 'pied-piper-pp', 'snapchat', 'snapchat', 'youtube', 'youtube', 'google-plus', 'google-plus'];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        return array;
     }
 
-    return array;
-}
+    shuffle(cards); // shuffle the list of cards
 
+    let html = "";
+    cards.forEach(function (item) { //loop through each card and create its HTML
+        html += `
+    <li class="card">
+      <i class="fa fa-${item}"></i>
+    </li>`;
+    });
+    document.getElementById("deck").innerHTML = html; // add html (the cards) to index.html
 
+};
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +40,5 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+init();
