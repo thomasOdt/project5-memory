89f7425933e6189e05d25db2a7ee20a20e3c453b
odtThomas <t.denotter@graafschapcollege.nl>
2017-11-23 12:48:41 +0100

    add scoreboard

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 let openCards = [];
-let moves =0;
+let moves = 0;
+let goodCards = 0;
 
 function init() {
-    //console.log(openCards);
-
+    
     //  Create a list that holds all of your cards based on there font-awesome name.
     let cards = ['facebook', 'facebook', 'twitter', 'twitter', 'twitch', 'twitch', 'instagram', 'instagram', 'pied-piper-pp', 'pied-piper-pp', 'snapchat', 'snapchat', 'youtube', 'youtube', 'google-plus', 'google-plus'];
 
 
     // Shuffle function from http://stackoverflow.com/a/2450976
     function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+        let currentIndex = array.length, temporaryValue, randomIndex;
 
         while (currentIndex !== 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,12 +25,10 @@ function init() {
 
     // shuffle the list of cards
     shuffle(cards);
-    console.log(cards);
 
     let html = "";
     cards.forEach(function (item) { //loop through each card and create its HTML
         html += `<li class="card"><i class="fa fa-${item} fa-2x"></i></li>`;
-        //html += `<li class="card open show"><i class="fa fa-${item} fa-2x"></i></li>`;
     });
 
     // add html (the cards) to index.html
@@ -51,10 +49,14 @@ function showCard(card) {
 
 function OpenCard(card) {
     openCards.push(card);
-    //console.log(openCards);
     if (openCards.length == 2) {
         if (openCards[0] == card) {
             setTimeout(cardsMatch,200);
+            goodCards+=2;
+            if(goodCards == 16  ){
+                // all cards are guested right
+                setTimeout(finished, 500);
+            }
         } else {
             cardsWrong();
             setTimeout(cardsClose,2000);
@@ -88,27 +90,28 @@ function addMoves() {
     }
 }
 
+function finished() {
+    $('.container').fireworks({
+        sound: true, // sound effect
+        opacity: 0.9,
+        height: '100%',
+        width: '100%'
+    });
+    $(".turn").text(moves);
+    setTimeout(showScoreboard,3000);
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function showScoreboard() {
+    $('.finish').fadeIn(500).removeClass("noShow");
+    // click to restart.
+    $('.finish').on('click', function(){
+        location.reload();
+    });
+}
 
-// when clicked on the restart button, init() runs
+// when clicked on the restart button, page reload so init() will run again.
 $(".restart").on('click',function(){
     location.reload();
 });
 
 init();
-
-
-
-
-//console.log(countInArray(openCards, "facebook")); // returns 2
-//console.log(countInArray(list, 1)); // returns 3
